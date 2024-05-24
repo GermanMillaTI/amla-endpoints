741823f2a0b8a9d84b66b56dd6645a52474fe74a
German Alejandro Milla Ortiz <german.milla01@telusinternational.com>
2024-05-24 13:42:35 -0600

    Initial setup

--- functions/main.py
+++ functions/main.py
@@ -4,14 +4,53 @@
 
 from firebase_functions import https_fn, options
 from firebase_admin import initialize_app
+import base64
+from werkzeug.datastructures import FileStorage
+from werkzeug.utils import secure_filename
+import io
+from pydub import AudioSegment
+import json
 
-# initialize_app()
-#
-#
+#initialize_app()
+
 @https_fn.on_request(
     cors=options.CorsOptions(
         cors_origins="*", 
-        cors_methods=["get", "post"]))
+        cors_methods=["post"]))
 
 def date(req: https_fn.Request) -> https_fn.Response:
-    """Get the server's local date and time."""
+
+    if req.method != "POST":
+        return https_fn.Response(json.dumps({"response": "Invalid request type, only POST requests are allowed"}), mimetype="application/json", status=405)
+    try:
+        content_type = req.headers.get('Content-Type', '')
+    except:
+        return https_fn.Response("Invalid content type in the request", status=500)
+    
+    if 'multipart/form-data' in content_type:
+        if 'audio' in req.files and req.files['audio'].filename != '':
+
+            filename = secure_filename(req.files['audio'].filename)
+
+            #reading file
+            fileBytes = req.files['audio'].read()
+            encodedBytes = base64.b64encode(fileBytes).decode('utf-8')
+
+            response_data = {
+                "audio": { 
+                "content": encodedBytes,
+                "extension": ".mp4"
+                },
+                "length": {
+
+                },
+                "result": 1
+            }
+
+            return https_fn.Response(json.dumps(response_data), mimetype="application/json", status=200)
+        else:
+            return https_fn.Response("No audio file in the request", status=400)
+    
+    #This return will execute if the request failed
+    return https_fn.Response("internal error", status=500)
+        
