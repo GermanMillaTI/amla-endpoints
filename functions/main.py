f6f3733edd856a7a1de5dfa0a807991f75b04a42
German Alejandro Milla Ortiz <german.milla01@telusinternational.com>
2024-05-27 13:32:45 -0600

    added result output 2

--- functions/main.py
+++ functions/main.py
@@ -8,10 +8,8 @@ import os
 from pydub import AudioSegment
 from pydub.silence import detect_nonsilent
 
-#initialize_app()
+initialize_app()
 
-
-#Pending: results 2, 3 and 4
 @https_fn.on_request(
     cors=options.CorsOptions(
         cors_origins="*", 
@@ -27,7 +25,7 @@ def extractAudioData(req: https_fn.Request) -> https_fn.Response:
         return https_fn.Response("Invalid content type in the request", status=500)
     
     if 'multipart/form-data' in content_type:
-        if 'audio' in req.files and req.files['audio'].filename != '':
+        try:
 
             filename, fileExtension = os.path.splitext(secure_filename(req.files['audio'].filename))
 
@@ -37,7 +35,7 @@ def extractAudioData(req: https_fn.Request) -> https_fn.Response:
             audioBinary = audiofile.read()
             aseg = AudioSegment.from_file(io.BytesIO(audioBinary))
 
-            #min silence len and silence thresh are made up numbers, so testing needs to be done to see what numbers are acceptable
+            
             non_silence_ranges = detect_nonsilent(aseg, min_silence_len=10, silence_thresh=-120)
             
             if non_silence_ranges:
@@ -47,7 +45,6 @@ def extractAudioData(req: https_fn.Request) -> https_fn.Response:
                 trimmed_audio.export(filename + fileExtension, format="mp4")
                 aftertrim = AudioSegment.from_file(filename + fileExtension)
 
-                #re assigment of binary file so it reads the new Audio Segment instead
                 with open(filename + fileExtension, "rb" ) as audiofile:
                     audioBinary = audiofile.read()
                 encodedBytes = base64.b64encode(audioBinary).decode('utf-8')
@@ -60,11 +57,15 @@ def extractAudioData(req: https_fn.Request) -> https_fn.Response:
                 "length": len(aftertrim) / 1000,
                 "result": 1
             }
-
-
             return https_fn.Response(json.dumps(response_data), mimetype="application/json", status=200)
-        else:
-            return https_fn.Response("No audio file in the request", status=400)
+
+        except Exception as e:
+            response_data = {
+                "result": 2,
+                "message": e
+            }
+            
+            return https_fn.Response(json.dumps(response_data), mimetype="application/json", status=400)
     
     #This return will execute if the request failed
     return https_fn.Response("internal error", status=500)
